f72fec571cbd5b1321724accaa15136cb15760f1
bchene <hey@hi-ben.net>
2025-11-19 21:47:46 +0100

    OOP ex01

--- Dev/Python_3_OOP/ex00/S1E9.py
+++ Dev/Python_3_OOP/ex00/S1E9.py
@@ -29,26 +29,3 @@ Stark(first_name: str, is_alive: bool = True).'''
         '''Stark method die(self) : \
 sets the is_alive attribute to False.'''
         self.is_alive = False
-
-# Ned = Stark("Ned")
-# >
-# print(Ned.__dict__)
-# > {'first_name': 'Ned', 'is_alive': True}
-# print(Ned.is_alive)
-# > True
-# Ned.die()
-# >
-# print(Ned.is_alive)
-# > False
-# print(Ned.__doc__)
-# > Stark Class is a subclass of Character for the Stark family.
-# print(Ned.__init__.__doc__)
-# > Constructor Method Stark(first_name: str, is_alive: bool = True).
-# print(Ned.die.__doc__)
-# > Stark method die(self) : sets the is_alive attribute to False.
-# print("---")
-# > ---
-# Lyanna = Stark("Lyanna", False)
-# >
-# print(Lyanna.__dict__)
-# > {'first_name': 'Lyanna', 'is_alive': False}
